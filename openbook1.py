2c99bd29258856625fd5fd65c5aac3512504bd44
sumit7207 <sumit@dal.ca>
2019-02-06 17:40:35 -0400

    complete openbook 1 test

--- openbook1.py
+++ openbook1.py
@@ -14,10 +14,41 @@ def list_ifname_ip(f):
 	for i in range(len(k)):
 		d.update({k[i]:v[i]})
 	return d
-def replace_ip(f):
-	for key in f:
-		f=f.replace("192","10")
-		f=f.replace("172","10")			#there is an error
+
 dic = list_ifname_ip(file)
 print (dic)
-print(replace_ip(dic))
+
+#task2
+f=open('running-config.cfg')
+def replace_ip(fin):
+	for line in fin:
+		line=line.strip()
+		line=line.replace("192","10")
+		line=line.replace("255.255.0.0","255.0.0.0")
+		line=line.replace("172","10")
+		line=line.replace("255.255.255.0","255.0.0.0")
+		print (line)
+
+replace_ip(f)
+
+
+#task3
+fil=open('running-config.cfg')
+def access_list(f):
+	transit_access_in=[]
+	fwmanagement_access_in=[]
+	global_access =[]
+	for line in f:
+		line=line.strip()
+		word=line.split()
+		if "transit_access_in" in word:
+			transit_access_in.append(line)
+		if "fw-management_access_in" in word:
+                        fwmanagement_access_in.append(line)
+		if "global_access" in word:
+                        global_access.append(line)
+	print (transit_access_in)
+	print(fwmanagement_access_in)
+	print(global_access)
+
+access_list(fil)
